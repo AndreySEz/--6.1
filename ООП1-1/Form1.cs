a0acf4054d3c4e4fb3185d432bf1b623cbbdd3ee
Andreus <Andreus@DESKTOP-I3EQQ7H>
2020-06-18 23:19:27 +0500

    Добавьте файлы проекта.

---
+++ ООП1-1/Form1.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ООП1_1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        public class Person
+        {
+            public string Name;
+            public string Surname;
+
+            public Person()
+            {
+                Name="";
+                Surname="";
+            }
+
+            public Person(string personName)
+            {
+                Name = personName;
+                Surname = "";
+            }
+
+            public Person(string personName, string personSurname)
+            {
+                Name = personName;
+                Surname = personSurname;
+            }
+        }
+    }
+}
